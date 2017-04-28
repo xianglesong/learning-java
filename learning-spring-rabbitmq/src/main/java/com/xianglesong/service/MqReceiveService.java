5a573250d4f971d1ce55c8e0d3fdc2b9a45ab855
xianglesong <rulinma@qq.com>
2017-04-28 11:23:49 +0800

    add msg convert.

--- learning-spring-rabbitmq/src/main/java/com/xianglesong/service/MqReceiveService.java
+++ learning-spring-rabbitmq/src/main/java/com/xianglesong/service/MqReceiveService.java
@@ -1,5 +1,6 @@
 package com.xianglesong.service;
 
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
@@ -12,36 +13,42 @@ import org.springframework.stereotype.Component;
 @Component
 public class MqReceiveService {
 
-    @RabbitListener(queues = "queue-1")
-    public void processMessage(String content) {
-        System.out.println("received: " + content);
-        // throw new RuntimeException("test");
-    }
+  @RabbitListener(queues = "queue-1")
+  public void processMessageM(Message messgage) {
+    String object = new String(messgage.getBody());
+    System.out.println("received: " + object);
+  }
+
+  @RabbitListener(queues = "queue-1")
+  public void processMessage(String content) {
+    System.out.println("received: " + content);
+    // throw new RuntimeException("test");
+  }
 
 
-//    @RabbitListener(queues = "queue-1")
-//    public void processMessage1(String content) {
-//        System.out.println("received 2: " + content);
-//    }
-//
-//    @RabbitListener(queues = "queue-2")
-//    public void processMessage2(String content) {
-//        System.out.println("received -2: " + content);
-//    }
+  // @RabbitListener(queues = "queue-1")
+  // public void processMessage1(String content) {
+  // System.out.println("received 2: " + content);
+  // }
+  //
+  // @RabbitListener(queues = "queue-2")
+  // public void processMessage2(String content) {
+  // System.out.println("received -2: " + content);
+  // }
 
-//    @RabbitListener(queues = "queue-3")
-//    public void processMessage3(String content) {
-//        System.out.println("received -3: " + content);
-//    }
+  // @RabbitListener(queues = "queue-3")
+  // public void processMessage3(String content) {
+  // System.out.println("received -3: " + content);
+  // }
 
-    @RabbitListener(queues = "queue-1f")
-    public void processMessagef(String content) {
-        System.out.println("received 1f: " + content);
-    }
+  @RabbitListener(queues = "queue-1f")
+  public void processMessagef(String content) {
+    System.out.println("received 1f: " + content);
+  }
 
-    @RabbitListener(queues = "queue-2f")
-    public void processMessage2f(String content) {
-        System.out.println("received 2f: " + content);
-    }
+  @RabbitListener(queues = "queue-2f")
+  public void processMessage2f(String content) {
+    System.out.println("received 2f: " + content);
+  }
 
 }
